225036f548e4545b0fbfcd514341d691c705e62a
DBouraoui <contact@dbouraoui.fr>
2025-12-03 12:06:16 +0100

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  return (
+    <>
+      <p className="text-blue-500">Hello</p>
+        <Button>Hello</Button>
+    </>
+  )
+}
+
+export default App
